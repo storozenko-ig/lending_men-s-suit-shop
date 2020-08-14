5e73fed420f0f9d59b84df681ab3acff1b8b0772
IgorSto <storozenko.ig@gmail.com>
2020-08-14 16:16:19 +1000

    first commit

---
+++ js/functions.js
@@ -0,0 +1,46 @@
+//Opening box with information about thing/form
+function openAndClosedBlock(classParent, elemSearch, openBlock, classNameClosed) {
+  [...document.querySelectorAll(classParent)].forEach((el) => {
+    let openBl = el.querySelector(openBlock);
+    let elem = el.querySelector(elemSearch);
+    elem.onclick = (e) => {
+      openBl.style.display = "block";
+    };
+    el.querySelector(classNameClosed).onclick = (e) => {
+      openBl.style.display = "none";
+    };
+  });
+}
+
+//Opening box form in information about thing
+function openFormFromOrder(classParent, elemSearch, openBlock, classNameClosed) {
+  [...document.querySelectorAll(classParent)].forEach((el) => {
+    let elem = el.querySelector(elemSearch);
+    let openBl = el.querySelector(openBlock);
+    let closed = el.querySelector(classNameClosed);
+    elem.onclick = (e) => {
+      openBl.style.display = "block";
+      closed.style.display = "none";
+    };
+  });
+}
+
+//Form call back
+const PHONE_MASK = "^(\\+7|8)[\\d]{10}$";
+const MAIL_MASK = "^[a-zA-z]+\\W?[a-z]+@[a-zA-z]+\\.[a-z]{2,3}$";
+const NAME_MASK = "^[a-zA-Zа-яА-Я'][a-zA-Zа-яА-Я-' ]+[a-zA-Zа-яА-Я']?$";
+
+function getMask(name) {
+  return name === "phone" ? PHONE_MASK : name === "email" ? MAIL_MASK : NAME_MASK;
+}
+
+function test(mask, domElem, domfildError, messageError, messageNotCorrect, className, classNameValue) {
+  if (domElem.value === "") {
+    domfildError.innerHTML = messageError;
+  } else if (new RegExp(getMask(mask)).test(domElem.value)) {
+    domfildError.innerHTML = "";
+    domElem.setAttribute(className, classNameValue);
+  } else {
+    domfildError.innerHTML = messageNotCorrect;
+  }
+}
---
+++ js/main.js
@@ -0,0 +1,53 @@
+//Opening box with infortion about thing
+openAndClosedBlock(".catalog-item", "img", ".open-info-item-wrapper", ".close");
+
+//Opening Form
+openAndClosedBlock(".catalog-item", ".buy-item-btn", ".popup-form", ".popur-form-closed");
+
+//Opening Form to order
+openFormFromOrder(".catalog-item", ".open-info-buy-btn", ".popup-form", ".open-info-item-wrapper");
+
+//Form call back
+//
+//messageName
+const nameError = "Поле обязательно для заполнения";
+const nameNotCorrect = "Проверьте поле имя";
+// messagePhone
+const phoneError = "Поле обязательно для заполнения";
+const phoneNotCorrect = "Некооректный номер телефона";
+//messageEmail
+const emailError = "Поле обязательно для заполнения";
+const emailNotCorrect = "Некооректный почтовый адрес";
+
+[...document.querySelectorAll(".popup-form-inner")].forEach((el) => {
+  let nameFildError = el.querySelector(".fild-error-one");
+  let phoneFildError = el.querySelector(".fild-error-two");
+  let emailFildError = el.querySelector(".fild-error-three");
+  el.querySelector(".fild-contener-bth").onclick = (evt) => {
+    evt.preventDefault();
+    for (let input of el.querySelectorAll(".input-all")) {
+      if (input.dataset.validatio === "name") {
+        test(input.dataset.validatio, input, nameFildError, nameError, nameNotCorrect);
+      } else if (input.dataset.validatio === "phone") {
+        test(input.dataset.validatio, input, phoneFildError, phoneError, phoneNotCorrect, "data-correct", "true");
+      } else if (input.dataset.validatio === "email") {
+        test(input.dataset.validatio, input, emailFildError, emailError, emailNotCorrect);
+      }
+    }
+    for (let input of document.querySelectorAll("[data-correct]")) {
+      if (input.dataset.correct) {
+        el.querySelector(".form").onsubmit = async () => {
+          let formData = new FormData(form);
+          let respons = await fetch("URL", {
+            method: "GET",
+            body: formData,
+          });
+          let result = await respons.json();
+          console.log(result);
+        };
+      } else {
+        return false;
+      }
+    }
+  };
+});
